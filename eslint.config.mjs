a08a6e0b87804a6871989583e0f2c5e5137f3a31
j-princi3 <jainprinci00@gmail.com>
2025-06-25 14:06:07 +0530

    first commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,11 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+
+  // Add ignore pattern here
+  {
+    ignores: ["lib/generated/prisma/**"],
+  },
 ];
 
 export default eslintConfig;
